99197f6df2845233310ffd3d130ceddd98acffd9
Magno Costa <magnocosta.br@gmail.com>
2016-01-26 20:36:39 -0200

    Add spec to file ConfigManager

---
+++ spec/app/util/doctor/config_manager_spec.rb
@@ -0,0 +1,23 @@
+require "spec_helper"
+
+RSpec.describe Doctor::ConfigManager do
+
+  subject do
+    described_class
+  end
+
+  context "When change attributes of object" do
+
+    it "Does return new value on attribute 'active_record_list'" do
+      subject.active_record_list << "hello"
+      subject.active_record_list << "say"
+      expect(subject.active_record_list).to match_array(["hello", "say"]) 
+    end
+
+    it "Does return new value on attribute 'url_to_telnet_list'" do
+      subject.url_to_telnet_list << "telnet"
+      subject.url_to_telnet_list << "list"
+      expect(subject.url_to_telnet_list).to match_array(["list", "telnet"]) 
+    end
+  end
+end
